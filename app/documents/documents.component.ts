26a7dc8332beb91e7564f455dfd393abc2ef55b7
jasminejeane <jasminejeane@gmail.com>
2017-01-11 22:55:02 -0800

    changes made

---
+++ app/documents/documents.component.ts
@@ -0,0 +1,34 @@
+import {Component} from '@angular/core';
+import {Document} from './document'
+
+@Component({
+  moduleId: module.id,
+  selector: 'documents',
+  templateUrl: 'documents.component.html'
+})
+export class DocumentsComponent {
+  documents : Document[] =[
+    {
+      title: "My first doc",
+      description: "vbzdfbnjfkzlbv;ksfpov;",
+      file_url: "http://google.com",
+      updated_at: '1/11/17',
+      image_url: "http://google.com",
+    },
+    {
+      title: "My second doc",
+      description: "vbzdfbnjfkzlbv;ksfpov;",
+      file_url: "http://google.com",
+      updated_at: '1/11/17',
+      image_url: "http://google.com",
+    },
+    {
+      title: "My third doc",
+      description: "vbzdfbnjfkzlbv;ksfpov;",
+      file_url: "http://google.com",
+      updated_at: '1/11/17',
+      image_url: "http://google.com",
+    }
+  ]
+
+}
