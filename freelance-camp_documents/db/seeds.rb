a684c857ea8278754a616ff4bce3735ef2e2f20a
jasminejeane <jasminejeane@gmail.com>
2017-01-14 14:01:56 -0800

    created moc api and set up rails app

---
+++ freelance-camp_documents/db/seeds.rb
@@ -0,0 +1,18 @@
+# export interface Document {
+#   title: string,
+#   description: string,
+#   file_url: string,
+#   updated_at: string,
+#   image_url: string,
+# }
+
+10.times do |d|
+FreelanceDocument.create!(
+title: "Document #{d}", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla t in culpa qui officia deserunt mollit anim id est laborum.",
+file_url: "https://docs.google.com/document/d/1UUgAob6ZpivgkgCbMD84JMMRc1flrzCPKybtQoAfASo/edit?usp=sharing",
+image_url: 'https://s3.amazonaws.com/devcamp-static/images/freelance-img.jpg'
+)
+end
