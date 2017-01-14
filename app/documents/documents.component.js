ce17f88943c41727f7aebc6217d1497d66ceda47
jasminejeane <jasminejeane@gmail.com>
2017-01-14 12:28:23 -0800

    changes made

--- app/documents/documents.component.js
+++ app/documents/documents.component.js
@@ -5,7 +5,10 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var core_1 = require("@angular/core");
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
 var DocumentsComponent = (function () {
     function DocumentsComponent() {
         this.pageTitle = "Document Dashboard";
@@ -33,15 +36,16 @@ var DocumentsComponent = (function () {
             }
         ];
     }
+    DocumentsComponent = __decorate([
+        core_1.Component({
+            moduleId: module.id,
+            selector: 'documents',
+            templateUrl: 'documents.component.html',
+            styleUrls: ['documents.component.css']
+        }), 
+        __metadata('design:paramtypes', [])
+    ], DocumentsComponent);
     return DocumentsComponent;
 }());
-DocumentsComponent = __decorate([
-    core_1.Component({
-        moduleId: module.id,
-        selector: 'documents',
-        templateUrl: 'documents.component.html',
-        styleUrls: ['documents.component.css']
-    })
-], DocumentsComponent);
 exports.DocumentsComponent = DocumentsComponent;
 //# sourceMappingURL=documents.component.js.map
