acf5d14f73ee931b06067f7c688684512dac0d9f
jasminejeane <jasminejeane@gmail.com>
2017-01-15 14:57:44 -0800

    changes made

--- freelance-camp-fe/app/app.module.ts
+++ freelance-camp-fe/app/app.module.ts
@@ -5,7 +5,6 @@ import { HttpModule } from '@angular/http';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import {AppRoutingModule} from './app-routing.module';
-
 import {AppComponent} from './app.component';
 import {HomepageComponent} from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
@@ -13,9 +12,7 @@ import { DocumentService } from './documents/document.service';
 import { ProposalListComponent } from './proposal/proposal-list.component';
 import { ProposalNewComponent } from './proposal/proposal-new.component';
 import { ProposalShowComponent } from './proposal/proposal-show.component';
-
-
-
+import { ProposalService } from './proposal/proposal.service';
 
 
  @NgModule({
@@ -35,7 +32,8 @@ import { ProposalShowComponent } from './proposal/proposal-show.component';
     ProposalShowComponent
   ],
   providers: [
-    DocumentService
+    DocumentService,
+    ProposalService
   ],
 
   bootstrap: [
